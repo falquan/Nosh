08690fa22552950dacb1fe340c879c6f38977734
Greg Leppert <falquan@falquan.com>
2016-04-04 09:57:33 -0700

    Add Feedbag code files

---
+++ Feedbag/Feedbag.swift
@@ -0,0 +1,9 @@
+//
+//  Feedbag.swift
+//  Nosh
+//
+//  Created by Falquan on 4/4/16.
+//  Copyright © 2016 Falquan. All rights reserved.
+//
+
+import Foundation
---
+++ FeedbagTests/FeedbagTests.swift
@@ -0,0 +1,9 @@
+//
+//  FeedbagTests.swift
+//  Nosh
+//
+//  Created by Falquan on 4/4/16.
+//  Copyright © 2016 Falquan. All rights reserved.
+//
+
+import Foundation
